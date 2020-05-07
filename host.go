e640b81307b99841bff5d08da1a0b6d428b4b84e
0xBahamoot <lam@autonomous.nyc>
2020-05-07 12:30:01 +0700

    update

--- host.go
+++ host.go
@@ -14,20 +14,24 @@ import (
 )
 
 type Host struct {
-	host          host.Host
-	NATType       byte
+	host          *host.Host
+	natType       network.Reachability
 	broadcastAddr string
 	intPort       int
 	natDevice     *NAT
-	natTypeChange chan byte
 	natMapping    Mapping
+	cancel        context.CancelFunc
 }
 
-func CreateHost(port int, NATdiscoverAddr string, autoNATService bool) *Host {
+func CreateHost(port int, NATdiscoverAddr string, autoNATService bool) (*Host, error) {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
-	var host Host
+	host := Host{
+		natType:       network.ReachabilityUnknown,
+		broadcastAddr: "",
+		intPort:       port,
+		cancel:        cancel,
+	}
 	var hostAddrStr string
 
 	natDevice, err := checkNATDevice()
@@ -39,12 +43,12 @@ func CreateHost(port int, NATdiscoverAddr string, autoNATService bool) *Host {
 		host.natDevice = natDevice
 		natMapping, err := createMapping(natDevice)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		host.natMapping = natMapping
 		hostAddr, err := natDevice.GetInternalAddress()
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		hostAddrStr = hostAddr.String()
 	}
@@ -56,29 +60,29 @@ func CreateHost(port int, NATdiscoverAddr string, autoNATService bool) *Host {
 
 	h, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/"+hostAddrStr+"/tcp/"+strconv.Itoa(port)), libp2p.EnableNATService())
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if autoNATService {
 		dialback, err := libp2p.New(ctx, libp2p.NoListenAddrs)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		_, err = autonat.New(ctx, h, autonat.EnableService(dialback.Network()))
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 	}
 
 	if NATdiscoverAddr != "" {
 		serviceInf, err := peerInfoFromString(NATdiscoverAddr)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		h.Peerstore().AddAddrs(serviceInf.ID, serviceInf.Addrs, time.Hour)
 		err = h.Connect(ctx, h.Peerstore().PeerInfo(serviceInf.ID))
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		go func() {
 			cSub, err := h.EventBus().Subscribe(new(event.EvtLocalReachabilityChanged))
@@ -92,13 +96,37 @@ func CreateHost(port int, NATdiscoverAddr string, autoNATService bool) *Host {
 				if stat == network.ReachabilityUnknown {
 					panic("After status update, client did not know its status")
 				}
-				fmt.Println(stat)
-			case <-time.After(30 * time.Second):
-				panic("sub timed out.")
+				host.natType = stat.(network.Reachability)
+			case <-ctx.Done():
+				return
 			}
 		}()
 
 	}
 
-	return &host
+	return &host, nil
+}
+
+func (h *Host) GetNATType() network.Reachability {
+	return h.natType
+}
+
+func (h *Host) GetBroadcastAddr() string {
+	return h.broadcastAddr
+}
+
+func (h *Host) GetNATDevice() *NAT {
+	return h.natDevice
+}
+
+func (h *Host) GetHost() *host.Host {
+	return h.host
+}
+
+func (h *Host) GetMapping() Mapping {
+	return h.natMapping
+}
+
+func (h *Host) Quit() {
+	h.cancel()
 }
